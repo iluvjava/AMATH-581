e64a30a10802f70020dc4da82c2c431248c01a82
Alto Legato <victor1301166040@gmail.com>
2020-10-28 20:11:52 -0700

    d

---
+++ HW2/HW2Trial1/P2Direct.m
@@ -0,0 +1,142 @@
+%% Problem 2
+
+L = 20; 
+DeltaX = 0.01; 
+Xs = -L: DeltaX: L;
+
+Band1Band3 = -ones(1, length(Xs) - 1); 
+Band2 = 2 + (DeltaX.*Xs).^2;
+
+% OptMatrix = spdiags(Band1Band3, - 1, 81, 81) + spdiags(Band1Band3, 1, 81, 81) + spdiags(Band2);
+OptMatrix = spdiags([Band1Band3, Band2, Band1Band3], -1:1, 81, 81);
+% OptMatrix(1, 1:3) = [0, -2/3, 2/3];
+OptMatrix(1, 1:3) = ([-3, 4, -1]/2)*DeltaX^2;
+% OptMatrix(1, end - 2: end) = [2/3, -2/3, 0];
+OptMatrix(1, end - 2: end) = ([1, -4, 3]/2)*DeltaX^2;
+OptMatrix = OptMatrix./DeltaX
+
+[V, D] = eigs(OptMatrix, 5, "smallestabs");
+hold on;
+for Col = V
+    plot(Xs, abs(Col));
+end
+
+% Refine the boundary values: 
+ForloopCount = 1;
+for Col = V
+    
+end
+
+%% Problem 2 A very good Computation Restults: 
+clear variables; 
+L = 8;
+DeltaX = 0.001; 
+Xs = -L: DeltaX: L;
+
+Band1Band3 = -ones(length(Xs), 1); 
+Band2 = 2 + (DeltaX.*Xs).^2; 
+Band2 = Band2';
+OptMatrix = spdiags... 
+    ([Band1Band3, Band2, Band1Band3], -1:1, length(Band2), length(Band2));
+OptMatrix(1, 1:3) = [-3, 4, -1]/(2*DeltaX);
+OptMatrix(end, end - 2: end) = [1, -4, 3]/(2*DeltaX);
+
+[V, D] = eigs(OptMatrix, 5, "smallestabs");
+disp(D/DeltaX^2);
+hold on;
+for Col = V
+    plot(Xs, abs(Col));
+end
+
+
+%% Problem 2 BootStrap 
+clear variables; 
+L = 4; 
+DeltaX = 0.1; 
+Xs = -L: DeltaX: L;
+
+Band1Band3 = -ones(length(Xs), 1); 
+Band2 = 2 + (DeltaX.*Xs).^2; 
+Band2 = Band2';
+OptMatrix = spdiags... 
+    ([Band1Band3, Band2, Band1Band3], -1:1, length(Band2), length(Band2));
+OptMatrix(1, 1:3) = [1, -2 + DeltaX*Xs(1)^2, 1];
+OptMatrix(end, end - 2: end) = [1, -2 + DeltaX*Xs(end)^2, 1];
+% OptMatrix(end, end - 2: end) = [1, -4, 3]/(2*DeltaX);
+
+[V, D] = eigs(OptMatrix, 5, "smallestabs");
+D = D/DeltaX^2;  % Should be this. 
+hold on;
+for Col = V
+    plot(Xs, abs(Col));
+end
+
+%% Problem 2 everuything follows from the PPT. 
+clear variables
+L = 4; DeltaX = 0.1; Xs = -L + DeltaX: DeltaX: L - DeltaX; 
+XsFull = [-L, Xs, L];
+
+Band1Band3 = -ones(length(Xs), 1); 
+Band2 = 2 + (DeltaX.*Xs).^2; 
+Band2 = Band2';
+OptMatrix = spdiags... 
+    ([Band1Band3, Band2, Band1Band3], -1:1, length(Band2), length(Band2));
+OptMatrix(1, 1:2) = [2/3 + (DeltaX*Xs(1))^2, -2/3];
+OptMatrix(end, end - 1: end) = [-2/3, (DeltaX*Xs(end))^2 + 2/3];
+[V, D] = eigs(OptMatrix, 5, "smallestabs");
+
+D = D/DeltaX^2;  % Should be this. 
+
+figure; hold on;
+for Col = V
+    plot(Xs, abs(Col));
+end
+title("No Bootstrap");
+
+figure; hold on;
+BSNormAbsEigFxns = ... 
+    zeros(size(V, 1)+ 2, size(V, 2));
+for I = 1: size(V, 2)
+    BSNormAbsEigFxns(:, I)...
+        = abs(BootStrap(V(:, I), L, D(I, I), DeltaX));
+    plot(XsFull, BSNormAbsEigFxns(:, I));
+end
+title("Bootstrap with BC ");
+% Save results for the HW. 
+
+A7  = BSNormAbsEigFxns(:, 1); 
+A8  = BSNormAbsEigFxns(:, 2); 
+A9  = BSNormAbsEigFxns(:, 3); 
+A10 = BSNormAbsEigFxns(:, 4); 
+A11 = BSNormAbsEigFxns(:, 5); 
+A12 = diag(D);
+
+%% Problem 2 but trimmed without any plotting. 
+clear variables
+L = 4; DeltaX = 0.1; Xs = -L + DeltaX: DeltaX: L - DeltaX; 
+
+Band1Band3 = -ones(length(Xs), 1); 
+Band2 = 2 + (DeltaX.*Xs).^2; 
+Band2 = Band2';
+OptMatrix = spdiags... 
+    ([Band1Band3, Band2, Band1Band3], -1:1, length(Band2), length(Band2));
+OptMatrix(1, 1:2) = [2/3 + (DeltaX*Xs(1))^2, -2/3];
+OptMatrix(end, end - 1: end) = [-2/3, (DeltaX*Xs(end))^2 + 2/3];
+[V, D] = eigs(OptMatrix, 5, "smallestabs");
+
+D = D/DeltaX^2;  % Should be this. 
+
+BSNormAbsEigFxns = ... 
+    zeros(size(V, 1)+ 2, size(V, 2));
+for I = 1: size(V, 2)
+    BSNormAbsEigFxns(:, I)...
+        = abs(BootStrap(V(:, I), L, D(I, I), DeltaX));
+end
+% Save results for the HW. 
+
+A7  = BSNormAbsEigFxns(:, 1);
+A8  = BSNormAbsEigFxns(:, 2);
+A9  = BSNormAbsEigFxns(:, 3);
+A10 = BSNormAbsEigFxns(:, 4);
+A11 = BSNormAbsEigFxns(:, 5);
+A12 = diag(D);
