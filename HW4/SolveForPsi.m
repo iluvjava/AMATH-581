7a0e3b990fbbd986d5fb0f4da093cc414818a113
Alto Legato <victor1301166040@gmail.com>
2020-11-19 01:56:15 -0800

    d

--- HW4/SolveForPsi.m
+++ HW4/SolveForPsi.m
@@ -5,20 +5,48 @@ function Psi = SolveForPsi(params, w)
     L = params.L;
     U = params.U; 
     P = params.P;
+    tic;
     switch params.SolveModes
         case 1 % BackSlash
             Psi = A2\w;
-            return
+            
         case 2 % LU
             Psi = U\(L\(P*w));
-            return
+            
         case 3 % Biconjugate Gradient Stabalized method. 
-            Psi = bicgstab(A2, w);
-            return
+            Psi = bicgstab(A2, w, params.BicgstabTOL);
+            
         case 4 % GMRES 
-            Psi = gmres(A2, w);
-            return
+            Psi = gmres(A2, w, [] ,params.GemresTOL);
+            
         case 5 % FFT 2D Solve
-            return 
+            N = params.n; 
+            L = params.l;  % L changed, not the L for LU decomposition anymore
+            w = reshape(w, N, N);
+            WFourier = fft2(w);
+            kx = fftshift((2*pi/L).*(-N/2: (N/2 - 1)));
+            kx(1) = 1e-6;
+            ky = kx';
+            Psi = real(ifft2(-WFourier./(kx.^2 + ky.^2)));
+            Psi = reshape(Psi, N*N, 1);
     end
+    TimeTransgressed = toc;
+    params.TimeStats.add(TimeTransgressed);
 end
+
+% function trimmed = TrimOffBoundary(w, n)
+%   % Trimm off the periodic boundary condition so that the vector is
+%   % suitable for FFT transform with periodic boundary condition.
+%   Matrix = reshape(w, [n, n]);
+%   Matrix = Matrix(1:end - 1, 1: end - 1);
+%   trimmed = reshape(Matrix, n - 1, n - 1);
+% 
+% end
+% 
+% function PadBoundary = PadBoundary(w, n)
+%   % Padd repeated rows and coumn and then add them back to the vector.
+%   Matrix = reshape(w, [n - 1, n - 1]);
+%   Matrix(:, end + 1) = Matrix(:, 1);
+%   Matrix(end + 1, :) = Matrix(1, :);
+%   PadBoundary = reshape(Matrix, n*n, 1);
+% end
---
+++ HW4/solution.m
@@ -0,0 +1,20 @@
+% Homework MATLAB template file
+% Your main file should be named "solution.m" and it should be saved as UTF-8 file.
+
+function [consoleout, A1, A2, A3, A4, A5] = solution()
+ [consoleout, A1, A2, A3, A4, A5] = evalc('student_solution(0)'); 
+end
+
+function [A1, A2, A3, A4, A5] = student_solution(dummy_argument)
+    
+    % your solution code goes here
+    
+    % assign the variables you are asked to save here  
+    A1 = 0; 
+    A2 = 0;
+    A3 = 0;
+    A4 = 0;
+    A5 = 0;
+end
+
+% your extra functions, if you need them, can be in other files (don't forget to upload them too!)
